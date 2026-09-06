329b3d059ae6b6ef60e08c718007b93c537676a3
diegoromarioH <diegoromario.work@gmail.com>
2026-09-06 03:28:13 -0600

    Refresh PWA cache for prealert design

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oex-rastreo-v2";
+const CACHE_NAME = "oex-rastreo-v3";
 const APP_SHELL = ["/", "/rastreo", "/manifest.webmanifest", "/oex-icon.svg", "/favicon.png"];
 
 self.addEventListener("install", (event) => {
